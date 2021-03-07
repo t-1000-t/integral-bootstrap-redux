d9d8525841943ca79cea138a85a02ed088a00a9d
t-1000-t <goognya@gmail.com>
2021-03-07 23:29:26 +0200

    new 6

--- src/components/Page/MainBoots/ViewCategoryProducts/ViewCategoryProducts.jsx
+++ src/components/Page/MainBoots/ViewCategoryProducts/ViewCategoryProducts.jsx
@@ -4,7 +4,6 @@ import axios from 'axios'
 import CardProduct from "./CardProduct"
 import closeDropdown from "../../../middleware/closeDropdown"
 import routes from "../../../../routes";
-import navIsShowCatalogReducer from "../../../services/ServiceHooks/allReducers/navIsShowCatalogReducer";
 import forGetArrProductsReducer
     from "../../../services/ServiceHooks/allReducers/forGetArrProductsReducer/forGetArrProductsReducer";
 
