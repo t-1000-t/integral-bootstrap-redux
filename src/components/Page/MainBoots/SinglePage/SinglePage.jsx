ecac97088178dce7050eeeb8a6e00e18d132fde5
t-1000-t <goognya@gmail.com>
2021-03-07 15:15:22 +0200

    fix single page & MainBoots

--- src/components/Page/MainBoots/SinglePage/SinglePage.jsx
+++ src/components/Page/MainBoots/SinglePage/SinglePage.jsx
@@ -1,66 +1,18 @@
-import React, {useState, useEffect, useReducer, Fragment} from 'react'
-import axios from "axios"
-import routes from "../../../../routes"
-import MainCard from "../MainCard";
-import mainReducer from "../../../services/ServiceHooks/allReducers/mainReducer";
+import React from 'react'
+
 
 export default function SinglePage() {
-    const [query, setQuery] = useState('main')
-    const [isLoading, setIsLoading] = useState(false)
-    const [isError, setIsError] = useState(false)
-    const [arrayMain, dispatch] = useReducer(mainReducer, [])
-
-    useEffect(() => {
-        let source = axios.CancelToken.source()
-        const fetchData = async () => {
-            setIsLoading(true)
-            setIsError(false)
-            try {
-                const result = await axios(`${routes.URL}/api/${query}`, {
-                    cancelToken: source.token
-                })
-                console.log("AxiosCancel: got response SinglePage")
-                const {main} = result.data
-                dispatch({type: "getMain", payload: {main}})
-            } catch (error) {
-                if (axios.isCancel(error)) {
-                    console.log("AxiosCancel: caught cancel", error.message)
-                } else {
-                    setIsError(true)
-                }
-            }
-            setIsLoading(false)
-        }
-
-        fetchData()
-
-        return () => {
-            console.log("AxiosCancel: unmounting")
-            source.cancel()
-        }
-    }, [query])
-
     return (
         <main role="main" className="container p-1">
-            {isLoading && (
-                <div className="text-center">
-                    <div className="spinner-border m-5" role="status">
-                        <span className="sr-only">Loading...</span>
-                    </div>
-                </div>
-
-            )}
-            {isError && <div>Something went wrong ..</div>}
-            {!isLoading && (
                 <div className="album py-1 bg-white">
                     <div className="container">
                         <div className="row">
-                            {arrayMain.map((elem, idx) => (
-                                <MainCard key={idx} elem={elem}/>))}
+                            Настроен глобальный поиск...
+
+                           А здесь будет рулетка с рекламными обьявлениями... в процессе
                         </div>
                     </div>
                 </div>
-            )}
         </main>
     )
 }
--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,13 +1,11 @@
 export default {
-  URL: "https://integral-server-route.herokuapp.com",
-  URLSearch: "https://req-server-boots.herokuapp.com",
+  URL: "https://intshop.herokuapp.com",
+  URLSearch: "https://intshop.herokuapp.com",
   HOME: "/",
-  MAIN: "/main",
   CONTACT: "/contact",
   PRODUCT: "/product",
   PRODUCT_CODE: "/product/product_code",
   PRODUCTS: "/products",
-  FILTERDB: "/filterdb",
   SEARCH: "/search",
   FEEDBACK: "/feedback"
   //   PRODUCTS_DETAILS: "/products/:someone"
