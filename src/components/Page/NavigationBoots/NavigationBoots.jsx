b4617aaab401906db25cd7cf3a3873f80d09133c
t-1000-t <goognya@gmail.com>
2021-03-31 12:49:34 +0300

    add logo

--- src/components/Page/NavigationBoots/NavigationBoots.jsx
+++ src/components/Page/NavigationBoots/NavigationBoots.jsx
@@ -4,10 +4,11 @@ import categ_list from "../../../services/categ_list";
 import "./NavigationBoots.css"
 import {
     BrowserRouter as Router,
-    Link
+    Linkgit add
 } from "react-router-dom";
 import listBurger from "../../../routes/routeBurger";
-
+import routes from "../../../routes/routes";
+import logo from "../../../images/logo/logo-61x61.jpg"
 
 function NavigationBoots() {
 const [isTouchPC, setTouchPC] = useState(false)
@@ -20,22 +21,9 @@ const [isTouchPC, setTouchPC] = useState(false)
 
     return (
         <>
-            <a href="/#" className="header__logo">
-                <svg
-                    width="1.5em"
-                    height="1.5em"
-                    viewBox="0 0 19 12"
-                    className="bi bi-laptop"
-                    fill="currentColor"
-                    xmlns="http://www.w3.org/2000/svg"
-                >
-                    <path
-                        fillRule="evenodd"
-                        d="M13.5 3h-11a.5.5 0 0 0-.5.5V11h12V3.5a.5.5 0 0 0-.5-.5zm-11-1A1.5 1.5 0 0 0 1 3.5V12h14V3.5A1.5 1.5 0 0 0 13.5 2h-11z"
-                    />
-                    <path d="M0 12h16v.5a1.5 1.5 0 0 1-1.5 1.5h-13A1.5 1.5 0 0 1 0 12.5V12z"/>
-                </svg>
-            </a>
+            <Link to={routes.HOME}>
+                <img className="header__logo" src={logo} alt="Integral"/>
+            </Link>
 
             <div className="header__menu menu">
                 <div className="menu__icon">
